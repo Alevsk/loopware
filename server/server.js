07be952278fdc23a850861e615a00fecb3bedcca
Lenin Alevski <alevski@freeagentsoftware.com>
2017-10-15 15:12:45 -0500

    index page for loopware server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var concat = require('concat-stream');
 var Stream = require('stream').Transform;
-var fs = require('fs')
+var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 3000;
+var hbs = require('express-handlebars');
+var path = require('path');
+var uuid = require('node-uuid')
 
 app.listen(port);
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts',
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'loopware',
+    uuid: uuid.v4(),
+  });
+});
 
 app.use (function(req, res, next) {
     req.rawBody = new Stream();
@@ -16,7 +35,6 @@ app.use (function(req, res, next) {
 });
 
 app.post('/havefun', function (req, res, next) {
-  const uuid = require('node-uuid');
   const reponse = {
     uuid: uuid.v4(),
   };
